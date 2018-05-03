181b04d264f012d8dc96b67a5a1f2b2b6fb5fba0
Grégoire Lurton <gregoire.lurton@gmail.com>
2018-05-03 20:51:49 +0200

    Get osm data from exernal directory

--- src/data/extract_osm.py
+++ src/data/extract_osm.py
@@ -3,7 +3,7 @@ from osmread import parse_file, Node
 import pandas as pd
 
 places = []
-for entity in parse_file('../../data/niger-latest.osm.pbf'):
+for entity in parse_file('../../data/external/niger-latest.osm.pbf'):
     if isinstance(entity, Node) and 'name' in entity.tags:
         place = {'lon':entity.lon , 'lat':entity.lat , 'name':entity.tags['name']}
         places.append(place)
