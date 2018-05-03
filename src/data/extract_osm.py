849a6ef9fa67a5d941f1b8c5786c99b4d7cea4a6
Grégoire Lurton <gregoire.lurton@gmail.com>
2018-05-03 18:44:29 +0200

    Update OSM extraction for new structure

--- src/data/extract_osm.py
+++ src/data/extract_osm.py
@@ -2,14 +2,12 @@
 from osmread import parse_file, Node
 import pandas as pd
 
-highway_count = 0
-
 places = []
-for entity in parse_file('niger-latest.osm.pbf'):
+for entity in parse_file('../../data/niger-latest.osm.pbf'):
     if isinstance(entity, Node) and 'name' in entity.tags:
         place = {'lon':entity.lon , 'lat':entity.lat , 'name':entity.tags['name']}
         places.append(place)
 
 out_data_frame = pd.DataFrame(places)
 
-out_data_frame.to_csv('osm_data.csv')
+out_data_frame.to_csv('../../data/raw/osm_data.csv')
